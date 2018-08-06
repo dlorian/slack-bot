1674d8e9475504581c44d6b92e5fa28001ec43c1
Florian Dorau <f.dorau@ymail.com>
2018-08-06 13:49:00 +0200

    Get it basically running

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # slack-bot
 Simpel bot for Slack
+
+
+Use ngrok for development in order to expose the local server to an external address.
+
+```
+./ngrok http 3000
+```
--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const http = require('http');
+require('dotenv').config()
 
+const http = require('http');
 const port = process.env.PORT || 3000;
+
+const { WebClient } = require('@slack/client');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+
+const token = process.env.SLACK_TOKEN;
+
+const web = new WebClient(token);
+
+const postMessage = (sender, recipient, message) => {
+  // The RTM client can send simple string messages
+
+  web.chat.postMessage({
+    channel: recipient.id,
+    text: `<@${sender.id}|${sender.name}> wants to greet you. Here is what he sent:\n>${message}`
+  })
+    .then(res => {
+      console.log('Message sent: ', res.ts);
+    })
+    .catch(console.error);
+};
 
 // You must use a body parser for JSON before mounting the adapter
 app.use(bodyParser.json());
@@ -16,16 +36,27 @@ app.get('/greetings', (req, res) => {
 });
 
 app.post('/greetings', (req, res) => {
-    // Extract user and text
-    const regexp = /(<@.*?>) (.*)$/;
-    const match =  regexp.exec(req.body.text);
-    console.log(match);
-
+  // Extract user and text
+  const regexp = /(<@.*?>) (.*)$/;
+  const match = regexp.exec(req.body.text);
+  const sender = {
+    id: req.body.user_id,
+    name: req.body.user_name
+  };
+  
+  if (match.length === 3) {
     const user = match[1];
-    const text = match[2];
-  res.send(`Hello ${user}, ${text}`);
+    const message = match[2];
+
+    const list = web.im.list()
+      .then(res => {
+        const recipient = res.ims.find(el => user.indexOf(el.user) !== -1);
+        postMessage(sender, recipient, message);
+      })
+      .catch(console.err);
+  }
 });
 
 const server = app.listen(port, () => {
-    console.log('Express server listening on port %d', port);
+  console.log('Express server listening on port %d', port);
 });
